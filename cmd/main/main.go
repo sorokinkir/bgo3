24d6d9a4302213c183bd8bd966bd9b0f396df509
Sorokin Kirill <itari@yandex.ru>
2020-12-23 14:31:33 +0700

    Вызовы функций AddTransaction и SumByMCC

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sorokinkir/bgo3/pkg/card"
 	"time"
 )
@@ -9,21 +10,21 @@ func main() {
 	master := &card.Card{
 		Transactions: []*card.Transaction{
 			&card.Transaction{
-				Id:       1,
+				ID:       1,
 				Amount:   735.55,
 				DateTime: time.Now().Unix(),
 				MCC:      "5411",
 				Status:   "Операция в обработке",
 			},
 			&card.Transaction{
-				Id:       2,
+				ID:       2,
 				Amount:   2_000.00,
 				DateTime: time.Now().Unix(),
 				MCC:      "0000",
 				Status:   "Пополнения",
 			},
 			&card.Transaction{
-				Id:       3,
+				ID:       3,
 				Amount:   1_203.91,
 				DateTime: time.Now().Unix(),
 				MCC:      "5812",
@@ -33,11 +34,21 @@ func main() {
 	}
 
 	transactionItem := card.Transaction{
-		Id:       4,
+		ID:       4,
 		Amount:   50_000.00,
 		DateTime: time.Now().Unix(),
 		MCC:      "5533",
 		Status:   "Обработано",
 	}
+
+	fmt.Println("------ Пример использования AddTransaction ------")
 	card.AddTransaction(master, &transactionItem)
+
+	codesMCC := []string{
+		"5533",
+		"5812",
+	}
+	fmt.Println("------ Пример использования SumByMCC ------")
+	total := card.SumByMCC(master.Transactions, codesMCC)
+	fmt.Printf("Сумма составляет: %v руб.\n", total)
 }
