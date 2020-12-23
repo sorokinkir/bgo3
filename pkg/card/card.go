c68cc80f3294bb66c3bd92097f4f52c826614b7b
Sorokin Kirill <itari@yandex.ru>
2020-12-23 11:01:32 +0700

    Добавлена функция AddTransaction

--- pkg/card/card.go
+++ pkg/card/card.go
@@ -1,5 +1,7 @@
 package card
 
+import "fmt"
+
 type Transaction struct {
 	Id       int64
 	Amount   float64
@@ -10,4 +12,21 @@ type Transaction struct {
 
 type Card struct {
 	Transactions []*Transaction
+}
+
+func AddTransaction(card *Card, transaction *Transaction) {
+	// TODO дописать добавление элементов в слайс
+	fmt.Println(transaction)
+
+	for i, row := range card.Transactions {
+		fmt.Printf(
+			"(ID в слайсе %v), ID=%v, сумма=%v, код MCC=%v, время=%v, статус=%v\n",
+			i,
+			row.Id,
+			row.Amount,
+			row.MCC,
+			row.DateTime,
+			row.Status,
+		)
+	}
 }
